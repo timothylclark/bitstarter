7462f0eb65128bf5e71600264e53ceb773b39faf
Ubuntu <ubuntu@ip-10-145-212-167.ec2.internal>
2013-07-09 02:17:40 +0000

    changed buffer length to buf.length

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buf = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8", 0, 27));
+  response.send(buf.toString("utf-8", 0, buf.length));
 });
 
 var port = process.env.PORT || 5000;
